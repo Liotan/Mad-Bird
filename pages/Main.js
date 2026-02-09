88064f0a95665113de7b6a42f7523ac044c41109
Liotan <sandorliotan@gmail.com>
2026-02-09 17:59:53 +0300

    edit the path to media files

--- pages/Main.js
+++ pages/Main.js
@@ -1,6 +1,6 @@
 const renderMain = () => {
     return `<div class="main">
-                <img src="./../../media/img/bird.png" alt="bird image">
+                <img src="media/img/bird.png" alt="bird image">
                 <p>Welcome to <span>Mad</span> Bird game! <br>
                    Press <span>"Game"</span> to start!
                 </p>
