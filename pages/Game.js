f7a44de8a6219daa8da722f4ef4d8fa60123f48e
Liotan <sandorliotan@gmail.com>
2026-02-09 15:45:57 +0300

    edit the path to media files

--- pages/Game.js
+++ pages/Game.js
@@ -2,14 +2,14 @@ function renderGame() {
     return `<canvas id="canvas"></canvas>
 
             <div class="media">
-                <img id="background" src="../media/game_background.png">
-                <img id="player" src="../media/player.png">
-                <img id="waterBall" src="../media/waterBall.png">
-                <audio id="jump" src="../media/wings.mp3"></audio>
-                <audio id="charge" src="../media/charge.mp3"></audio>
-                <audio id="win" src="../media/win.mp3"></audio>
-                <audio id="lose" src="../media/lose.mp3"></audio>
-                <audio id="music" src="../media/background.mp3"></audio>
+                <img id="background" src="../media/img/game_background.png">
+                <img id="player" src="../media/img/player.png">
+                <img id="waterBall" src="../media/img/waterBall.png">
+                <audio id="jump" src="../media/audio/wings.mp3"></audio>
+                <audio id="charge" src="../media/audio/charge.mp3"></audio>
+                <audio id="win" src="../media/audio/win.mp3"></audio>
+                <audio id="lose" src="../media/audio/lose.mp3"></audio>
+                <audio id="music" src="../media/audio/background.mp3"></audio>
             </div>
             `
 }
